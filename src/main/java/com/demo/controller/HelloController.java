b13eea590ece718b3bb5109078fc400eaa087eca
ucheng.wang <ucheng.wang@gmail.com>
2013-05-18 08:46:29 +0800

    add HelloController

---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public ModelAndView hello() {
+		return new ModelAndView("hello", "message", "Hello World Spring 2.0 MVC!");
+		
+	}
+}
